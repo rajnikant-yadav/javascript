9b5550f61c0e7540c90656eb5d14044ed3898e5e
Rajnikant Yadav <rajnikanty61@gmail.com>
2022-10-11 23:35:52 +0530

    adding ex2

--- forEach.js
+++ forEach.js
@@ -1,10 +1,7 @@
+//example 1
 function func() {
-      
-// Original array
-// example 1
 const items = [12, 24, 36];
     const multarr = [];
-
     items.forEach(function (item) {
         multarr.push(item * item);
     });
@@ -12,4 +9,21 @@ const items = [12, 24, 36];
     return multarr
 }
 a=func();
+console.log(a)
+
+
+// example 2
+//  var func=() =>{
+// const items = ['a', "b", "c"];
+//     const objarr = [];
+//   items.forEach((item)=> {
+       
+//         var obj={key:item}
+//         objarr.push(obj);
+//     });
+
+//     return objarr
+// }
+// a=func();
+// console.log(a)
 
