d2c2319d6044911d67c8068c1b714a73fd5523a1
Rajnikant Yadav <rajnikanty61@gmail.com>
2022-10-12 21:44:16 +0530

    concat and join method

---
+++ concat_join.js
@@ -0,0 +1,77 @@
+// concat example1
+// var arr1=[1,2,3]
+// var arr2=arr1.concat(4,5,6)
+// console.log(arr2)
+
+// example 2
+// var arr1=["a","b","c"]
+// var arr2=["d","e","f"]
+// var arr3=[]
+// var arr=arr3.concat(arr1,arr2)
+// console.log(arr)
+
+
+// example3 
+// var arr1=["a","b","c"]
+// var arr2=["d","e","f"]
+// var arr3=[1,3,4,4]
+// var arr=arr3.concat(arr1,arr2)
+// console.log(arr) 
+
+
+
+
+// Join ex1
+// var joinarr=[1,2,3,4,5,6]
+// var temp=joinarr.join()
+// console.log(temp)
+// output 1,2,3,4,5,6
+
+//exp2
+// var joinarr=[1,2,3,4,5,6]
+// var temp=joinarr.join("")
+// console.log(temp)
+// output 123456
+
+// example3
+// var joinarr=[1,2,3,4,5,6]
+// var temp=joinarr.join(" ")
+// console.log(temp)
+//output 1 2 3 4 5 6
+
+//ex4
+// var joinarr=[1,2,3,4,5,6]
+// var temp=joinarr.join("/")
+// console.log(temp)
+// output 1/2/3/4/5/6
+
+
+// ex5
+// var joinarr=[1,2,3,4,5,6]
+// var temp=joinarr.join("or")
+// console.log(temp)
+// otput 1or2or3or4or5or6
+
+// ex6
+// var joinarr=[1,2,3,4,5,6]
+// var temp=joinarr.join(" and ")
+// console.log(temp)
+// output  1 and 2 and 3 and 4 and 5 and 6
+
+
+// ex6
+// var joinarr=[1,2,3,4,5,6]
+// var temp=joinarr.join("\n")
+// console.log(temp)
+/* output 
+1
+2
+3
+4
+5
+6 */
+
+// ex7
+var joinarr=[1,2,3,4,5,6]
+var temp=joinarr.join("\n")
+console.log(temp)
