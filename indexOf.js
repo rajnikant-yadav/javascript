b608b5cb3bd703e0c4d2090fd38b532451f52526
Rajnikant Yadav <rajnikanty61@gmail.com>
2022-11-18 23:29:10 +0530

    removing files

--- indexOf.js
+++
@@ -1,7 +0,0 @@
-var arr = ["Rahul", "Sonam", "Raj", "Sumit", "Raj"];
-var position1 = arr.indexOf("Raj");
-var position2 = arr.indexOf("Raj", 3);
-var position3 = arr.indexOf("Priti");
-console.log(position1);
-console.log(position2);
-console.log(position3);
--- reverse.js
+++
@@ -1,2 +0,0 @@
-var names = ["Rahul", "Sonam", "Sumit"];
- console.log(names.reverse())
