b53353607e43f1edf6b98f4a6ac3680e477cae30
Rajnikant Yadav <rajnikanty61@gmail.com>
2022-11-20 09:21:46 +0530

    changes in array concat

--- arrayMethods/concat.js
+++ arrayMethods/concat.js
@@ -1,14 +1,14 @@
 // concat example1
-var arr1=[1,2,3]
-var arr2=arr1.concat(4,5,6)
-console.log(arr2)
+// var arr1=[1,2,3]
+// var arr2=arr1.concat(4,5,6)
+// console.log(arr2)
 
 // example 2
-// var arr1=["a","b","c"]
-// var arr2=["d","e","f"]
-// var arr3=[]
-// var arr=arr3.concat(arr1,arr2)
-// console.log(arr)
+var arr1=["a","b","c"]
+var arr2=["d","e","f"]
+var arr3=[]
+var arr=arr3.concat(arr1,arr2)
+console.log(arr)
 
 
 // example3 
