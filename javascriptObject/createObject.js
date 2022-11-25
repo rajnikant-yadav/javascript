4004a893ca67e29489f24d5afde5270a653cd537
Rajnikant Yadav <rajnikanty61@gmail.com>
2022-11-25 23:40:17 +0530

    adding js object

---
+++ javascriptObject/createObject.js
@@ -0,0 +1,10 @@
+var fees = { };		// Declaring empty object
+fees['a'] = 10;	// initializing 
+fees['b'] = 20;
+fees['c'] = 30;
+/*
+fees.a = 100;
+fees.b = 200;
+fees.c = 300;
+*/
+console.log(fees['a']);
