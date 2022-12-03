03a68a3ab06d11f7e0d64b55e872e65e84e78040
Rajnikant Yadav <rajnikanty61@gmail.com>
2022-12-03 10:55:24 +0530

    fibonacci

---
+++ commonquestions/fibonacci.js
@@ -0,0 +1,18 @@
+// Is there a formula for the Fibonacci Series?
+// The Fibonacci sequence formula deals with the Fibonacci sequence, finding its missing terms. The Fibonacci formula is given as, Fn = Fn-1 + Fn-2, where n > 1.
+
+function fibob(n){
+    let first=0, second=1,next=0
+    for(let i=0;i<n;i++){
+        console.log(first)
+        next=first+second
+        first=second
+        second=next
+
+}
+}
+
+const N=5
+fibob(N)
+
+
