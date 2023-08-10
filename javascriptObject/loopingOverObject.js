9eb890d33a9b8783caa256aee3df9c5ed8b7b991
rajnikant-yadav <rajnikanty61@gmail.com>
2023-08-10 21:15:06 +0530

    looping ove object

---
+++ javascriptObject/loopingOverObject.js
@@ -0,0 +1,58 @@
+// Here are different ways to loop through properties of a JavaScript object:
+
+const nestedObject = {
+    firstName: "Rajnikant",
+    lastName: "Yadav",
+    age: 30,
+    address: {
+        street: "1234",
+        city: "City",
+        state: "State",
+        postalCode: "782345"
+    },
+    hobbies: ["Reading", "Cooking", "Gardening"],
+    contact: {
+        email: "test@example.com",
+        phone: "23679999"
+    },
+    createdBy: "Rajnikant Yadav"
+};
+
+// 1. Using for...in loop (for objects)
+for (const key in nestedObject) {
+    console.log(`${key}: ${nestedObject[key]}`);
+}
+
+// 2. Using Object.keys() (for objects)
+const keys = Object.keys(nestedObject);
+for (const key of keys) {
+    console.log(`${key}: ${nestedObject[key]}`);
+}
+
+// 3. Using Object.values() (for objects)
+const values = Object.values(nestedObject);
+for (const value of values) {
+    console.log(value);
+}
+
+// 4. Using Object.entries() (for objects)
+const entries = Object.entries(nestedObject);
+for (const [key, value] of entries) {
+    console.log(`${key}: ${value}`);
+}
+
+// 5. Using forEach() for array properties
+nestedObject.hobbies.forEach(hobby => {
+    console.log(`Hobby: ${hobby}`);
+});
+
+// 6. Using nested loops for deeply nested properties
+for (const key in nestedObject) {
+    if (typeof nestedObject[key] === "object") {
+        for (const nestedKey in nestedObject[key]) {
+            console.log(`${nestedKey}: ${nestedObject[key][nestedKey]}`);
+        }
+    } else {
+        console.log(`${key}: ${nestedObject[key]}`);
+    }
+}
