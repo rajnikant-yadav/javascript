b9a54cbe0d2d5f058239a6ad55b58901485f9b2b
rajnikant-yadav <rajnikanty61@gmail.com>
2023-11-15 23:56:49 +0530

    toString method

--- arrayMethods/toString.js
+++ arrayMethods/toString.js
@@ -1,7 +1,18 @@
+// In JavaScript, the toString method is a built-in method that is available for most objects. Here's the syntax and a short description:
+// Syntax:  object.toString()
+
 const a=[1,2,3,4,5,6,7,"a",'b']
 console.log(a.toString())
 // output 1,2,3,4,5,6,7,a,b
 var b = ["Rahul", "Sonam", "Sumit", "Raj", "Rohan"];
 console.log(b.toString());
 
-// output       Rahul,Sonam,Sumit,Raj,Rohan
+// output Rahul,Sonam,Sumit,Raj,Rohan
+
+const number = 42;
+const array = [1, 2, 3];
+const date = new Date();
+
+console.log(number.toString()); // Output: 42
+console.log(array.toString());  // Output: 1,2,3
+console.log(date.toString());   // Output: e.g., Tue Nov 15 2023 12:34:56 GMT+0000 (Coordinated Universal Time)
