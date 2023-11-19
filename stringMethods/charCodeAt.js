22b5da91189354d79a8ea4f5474123867ecff3bf
rajnikant-yadav <rajnikanty61@gmail.com>
2023-11-19 07:23:43 +0530

    charCodeAt

--- stringMethods/charCodeAt.js
+++ stringMethods/charCodeAt.js
@@ -1,6 +1,7 @@
+// charCodeAt()
+// Syntax: str.charCodeAt(index)
 // The charCodeAt() method returns the unicode of the character at a specified index in a string:
-
-// The method returns a UTF-16 code (an integer between 0 and 65535
+// The method returns a UTF-16 code (an integer between 0 and 65535)
 
 let text = "HELLO WORLD";
 let char = text.charCodeAt(3);
