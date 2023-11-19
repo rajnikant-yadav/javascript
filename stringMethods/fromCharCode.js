113518b9974efb9be045018f3678f666e7b01ce5
rajnikant-yadav <rajnikanty61@gmail.com>
2023-11-19 08:37:07 +0530

    String.fromCharCode

--- stringMethods/fromCharCode.js
+++ stringMethods/fromCharCode.js
@@ -1,7 +1,11 @@
+// String.fromCharCode() Method:
+
 // Function will return a string created by using specified sequence of unicode values(argc1,c2).
 
+// Syntax : String.fromCharCode(char1, char2, ..., charN)
 
-//fromCharCode(c1, c2,...)
+
+// fromCharCode(c1, c2,...)
 console.log(String.fromCharCode(97,98,99,120,121,122))
 //output: abcxyz
 console.log(String.fromCharCode(72,69,76,76,79))
