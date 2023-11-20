8e160519b562efab8ae5a067814816dbf227a8a8
rajnikant-yadav <rajnikanty61@gmail.com>
2023-11-20 10:50:40 +0530

    repeat

--- stringMethods/repeat.js
+++ stringMethods/repeat.js
@@ -1,7 +1,14 @@
 // repeat()
-// This returns a new string with a specified number of copies of an existing string.
+// In JavaScript, the repeat() method is a built-in method for strings that allows you to create a new string by repeating the original string a specified number of times. The syntax for the repeat() method is as follows:
+// string.repeat(count)
 
 //repeat()
 var string = "Welcome to Edureka";
 console.log(string.repeat(2));
 //output: Welcome to EdurekaWelcome to Edureka
+
+let originalString = "Hello ";
+let repeatedString = originalString.repeat(3);
+
+console.log(repeatedString);
+// Output: "Hello Hello Hello "
