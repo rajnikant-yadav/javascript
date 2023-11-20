269159a0df5f9081ca38754fe731781850761121
rajnikant-yadav <rajnikanty61@gmail.com>
2023-11-20 09:48:14 +0530

    indexof, match

--- stringMethods/indexOf.js
+++ stringMethods/indexOf.js
@@ -1,20 +1,12 @@
 // indexOf() Method
 // The indexOf( ) method takes a string argument and returns the index of the first occurance of the argument in the string. If the argument is not found returns -1 . This method also accepts an optional second argument that specifies the index at which to start the search. 
 // The indexOf() method cannot take powerful search values (regular expressions).
+
+// Syntax: string.indexOf(searchValue[, startIndex])
+
 var str = "Hi guys Lets Learn JavaScript";
 // console.log(str.indexOf("e"));	// 9
 // console.log(str.indexOf("e", 10));	//14
 // console.log(str.indexOf("Lets"));	// 8
 
-// search() Method
-// The search() method searches a string for a specified value and returns the position of the match 
-// The search() method cannot take a second start position argument.
-			
-// console.log(str.search("e"));
-// console.log(str.search("Lets"));
 
-// slice()
-// The slice() extracts a part of a string and returns the extracted part in a new string. The method takes 2 parameters: the starting index (position), and the ending index (position). The method returns a string containing the string beginning at the given index up to but not including the character at the index speficied by the second argument. If a parameter is negative, the position is counted from the end of the string.  
-			
-var str = "Hi guys Lets Learn JavaScript";
-console.log(str.slice(8, 14));	// 14 not included
--- stringMethods/match.js
+++ stringMethods/match.js
@@ -6,12 +6,18 @@
 // The match() method returns null if no match is found.
 
 // Syntax
-// string.match(match)
+// string.match(regexp)
 
 // let text = "The rain in SPAIN stays mainly in the plain";
 // const matched=text.match("ain");
+// console.log(matched)
+// Output:  [ 'ain', index: 5, input: 'The rain in SPAIN stays mainly in the plain', groups: undefined ]
+
+
 // let text = "The rain in SPAIN stays mainly in the plain";
 // const matched=text.match(/ain/);
+// Output : [ 'ain', 'ain', 'ain' ]
+
 
 // let text = "The rain in SPAIN stays mainly in the plain";
 // text.match(/ain/g);
@@ -20,3 +26,18 @@ let text = "The rain in SPAIN stays mainly in the plain";
 const matched=text.match(/ain/gi);
 
 console.log(matched)
+
+let str = "Hello, World!";
+let matches = str.match(/o/g);
+
+console.log(matches);
+// Output: [ 'o', 'o' ]
+
+// In this example, the regular expression /o/g is used to find all occurrences of the letter "o" in the string. The match() method returns an array ['o', 'o'] containing the matches.
+
+// If there are no matches, the method returns null:
+// let str = "Hello, World!";
+// let matches = str.match(/JavaScript/g);
+
+// console.log(matches);
+// // Output: null
