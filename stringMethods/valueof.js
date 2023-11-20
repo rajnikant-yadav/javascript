40e4c813acb793c2c8963f8c80df09a1eaa2f5d2
rajnikant-yadav <rajnikanty61@gmail.com>
2023-11-20 11:17:27 +0530

    valueOf

--- stringMethods/valueof.js
+++ stringMethods/valueof.js
@@ -1,3 +1,24 @@
+// In JavaScript, the valueOf() method is a method that is part of the prototype of all JavaScript objects. It is a generic method that returns the primitive value of the specified object. The valueOf() method is automatically called by JavaScript when an object is expected to be used as a primitive value, such as when using an object in a mathematical operation or when converting an object to a primitive type.
+
+// The basic syntax is:
 let text = "Hello World!";
 let result = text.valueOf();
 console.log(result)
+// outputL  Hello World!
+
+let myObject = {
+    value: 42,
+    valueOf: function() {
+      return this.value;
+    }
+  };
+  
+  let primitiveValue = myObject.valueOf();
+  console.log(primitiveValue); // Output: 42
+
+
+  // The valueOf() method is automatically called by JavaScript in certain situations where it needs to convert an object to a primitive value. For example:
+  let sum = myObject + 8;
+console.log(sum); // Output: 50
+
+// In this case, JavaScript automatically calls valueOf() on myObject to get its primitive value (which is 42) and then adds 8 to it.
