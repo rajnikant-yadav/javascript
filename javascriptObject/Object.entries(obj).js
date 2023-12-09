53aad950d457750355e78670639bc8ee82433a1d
rajnikant-yadav <rajnikanty61@gmail.com>
2023-12-09 20:52:43 +0530

    Object.fromEntries(entries)

--- javascriptObject/Object.entries(obj).js
+++ javascriptObject/Object.entries(obj).js
@@ -2,7 +2,20 @@
 
 // Example:
 
-const obj = { a: 1, b: 2, c: 3 };
-const entries = Object.entries(obj);
-console.log(entries);
+// const obj = { a: 1, b: 2, c: 3 };
+// const entries = Object.entries(obj);
+// console.log(entries);
 // Output: [['a', 1], ['b', 2], ['c', 3]]
+
+// The Object.fromEntries(entries) method in JavaScript performs the opposite operation of Object.entries. It takes an iterable of key-value pairs and returns a new object whose properties are defined by those pairs.
+
+// Parameters:
+
+// entries: An iterable object (e.g., array, map, set) of key-value pairs. Each pair should be an array of two elements: the first element being the key and the second element being the value.
+//Return value:
+
+//A new object where each key-value pair from the iterable defines a property in the returned object.
+
+const entries = [['name', 'John Doe'], ['age', 30], ['city', 'New York']];
+const person = Object.fromEntries(entries);
+console.log(person);
